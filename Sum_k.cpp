309eba404276fd2a94a92ed7ce0b9cfd6fee1be3
nhuhoang0701 <40765474+nhuhoang0701@users.noreply.github.com>
2019-05-11 14:14:51 +0200

    Update Sum_k.cpp

--- Sum_k.cpp
+++ Sum_k.cpp
@@ -1,31 +1,31 @@
-// Experiments.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "pch.h"
+// Sum_k.cpp : This file checks if there exists a sub-array which has sum equal k given.
 #include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
 
 int main()
 {
-	int n , k, sum = 0, j = 0, h, a; cin >> n;
-	int p[100];
+	int n, k, verification, sum = 0, j = 0, i = 0;
+	cin >> n;
+	int* p = new int[n];
 	for (int i = 0; i < n; i++)
-		cin >> p[i];
-	cin >> k;
-	for (int i = 0; i < n; i++)
+		cin >> p[i];			// Enter the elements of the array
+
+	cin >> k;	                    	// Enter the value we want to check
+
+	for (i; i < n; i++)
 	{
-		j = i;
-		while (sum < k && j<n)
+		j = i;								
+		while (sum < k && j < n)	// Check sum of the sub-array until it is bigger than k
 		{
-			sum += p[j]; a = i; j++;
+			sum += p[j]; j++;
 		}
 		if (sum == k)
 		{
-			h = 1; break;
+			verification = 1; i; break;
 		}
-		h = 0; sum = 0; j = 0;
+		verification = 0; sum = 0; j = 0;
 	}
-	cout << h <<" "<< a <<" "<< j - 1;
+	if (verification==1) cout << verification << " " << i << " " << j - 1; // Print the verification, the index of 2 extemity of the qualified array
+	else cout << verification;					       // Print h = 0 if such a sub-array exists
+	delete p;
 }
